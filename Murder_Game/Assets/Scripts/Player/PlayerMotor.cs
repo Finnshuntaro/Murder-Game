c2508e21836f5257ec1daf3286098b5f318a3a08
DeadWOrld23 <dead.world233@gmail.com>
2025-05-05 22:36:00 +1000

    Basic Footsteps Differentiation
    Made it so that that player now makes different footstep sound effects depending on the floor type. This is done through tags. It also doesn't play unless the player is walking on a solid object.

--- Murder_Game/Assets/Scripts/Player/PlayerMotor.cs
+++ Murder_Game/Assets/Scripts/Player/PlayerMotor.cs
@@ -10,6 +10,21 @@ public class PlayerMotor : MonoBehaviour
     public float gravity = -9.8f;
     public float jumpHeight = 1f;
 
+    [Header("Player Sound")]
+    [SerializeField] private bool useFootsteps;
+
+    [SerializeField] private float baseStepSpeed = 0.5f;
+    [SerializeField] private AudioSource footstepAudioSource = default;
+    [SerializeField] private AudioClip[] woodClips = default;
+    [SerializeField] private AudioClip[] carpetClips = default;
+    [SerializeField] private AudioClip[] defaultClips = default;
+
+    private float footstepTimer = 0;
+    private float GetCurrentOffSet => baseStepSpeed;
+
+    private Vector2 currentInput;
+    [SerializeField] private Transform playerCamera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,10 +35,14 @@ public class PlayerMotor : MonoBehaviour
     void Update()
     {
         isGrounded = controller.isGrounded;
+
+        Handle_Footsteps();
     }
 
     public void ProcessMove(Vector2 input)
     {
+        currentInput = input;
+
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
@@ -40,5 +59,40 @@ public class PlayerMotor : MonoBehaviour
     public bool IsGrounded()
     {
         return isGrounded;
+    }
+
+    private void Handle_Footsteps()
+    {
+        if (!useFootsteps) return;
+        if (!controller.isGrounded) return;
+        if (currentInput == Vector2.zero) return;
+
+        footstepTimer -= Time.deltaTime;
+
+        if (footstepTimer <= 0)
+        {
+            if (Physics.Raycast(playerCamera.position, Vector3.down, out RaycastHit hit, 3f))
+            {
+                Debug.Log("Raycast hit: " + hit.collider.name + ", tag: " + hit.collider.tag);
+
+                switch (hit.collider.tag)
+                {
+                    case "Footsteps/CARPET":
+                        footstepAudioSource.PlayOneShot(carpetClips[Random.Range(0, carpetClips.Length)]);
+                        break;
+                    case "Footsteps/WOOD":
+                        footstepAudioSource.PlayOneShot(woodClips[Random.Range(0, woodClips.Length)]);
+                        break;
+                    case "Footsteps/DEFAULT":
+                        footstepAudioSource.PlayOneShot(defaultClips[Random.Range(0, defaultClips.Length)]);
+                        break;
+                    default:
+                        footstepAudioSource.PlayOneShot(defaultClips[Random.Range(0, defaultClips.Length)]);
+                        break;
+                }
+            }
+
+            footstepTimer = GetCurrentOffSet; 
+        }
     }
 }
