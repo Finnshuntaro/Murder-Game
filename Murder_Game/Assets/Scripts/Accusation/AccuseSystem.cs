f473dd9127a9a708b4e5e88134ff61726147fa6c
DeadWOrld23 <dead.world233@gmail.com>
2025-05-22 08:29:23 +1000

    Hide Accuse Prompt
    Hidden the Accuse prompt until all npcs have been spoken to, added things to the prefab, added player material, changed textures location to be inside of materials

--- Murder_Game/Assets/Scripts/Accusation/AccuseSystem.cs
+++ Murder_Game/Assets/Scripts/Accusation/AccuseSystem.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class AccuseSystem : MonoBehaviour
 {
     public DialogueManager dialogueManager; // references the dialogueManager to see the isDialogueActive state
 
     // Assigning this in the inspector allows you to check each NPC 
     public NPCScript npc1; 
-    public NPCScript npc2; 
+    public NPCScript npc2;
+
+    public GameObject accuseUIPanel; // This UI (panel + text) only becomes visible once all NPCs have been spoken to
+
+    private bool hasActivatedUI = false; // Prevents UI from being activated repeatedly
+
+    void Start()
+    {
+        // At the beginning of the scene, the UI panel should not be visible
+        if (accuseUIPanel != null)
+        {
+            accuseUIPanel.SetActive(false);
+        }
+    }
 
     void Update()
     {
+        // If all NPCs have been spoken to, activate the UI panel (if it hasn’t been already)
+        if (!hasActivatedUI && npc1.hasSpoken && npc2.hasSpoken)
+        {
+            if (accuseUIPanel != null)
+            {
+                accuseUIPanel.SetActive(true);
+                hasActivatedUI = true; // Prevent re-triggering
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Q)) // The Button I plan to use to continue with the Accusation screen.
         {
+            // First, check if a dialogue is currently active
+            if (dialogueManager != null && dialogueManager.IsDialogueActive())
+            {
+                Debug.Log("You can’t proceed while a dialogue is active.");
+                return;
+            }
+
             // checks to see if all NPC's have been spoken to.
             if (npc1.hasSpoken && npc2.hasSpoken)
             {
-                // First, check if a dialogue is currently active
-                if (dialogueManager != null && dialogueManager.IsDialogueActive())
-                {
-                    Debug.Log("You can’t proceed while a dialogue is active.");
-                    return;
-                }
                 Debug.Log("All NPCs have been spoken to!");
                 ToAccuseScene();
             }
@@ -39,7 +62,6 @@ public class AccuseSystem : MonoBehaviour
     public void ToAccuseScene()
     {
         SceneManager.LoadScene(3);
-
         Cursor.lockState = CursorLockMode.None;
     }
 }
