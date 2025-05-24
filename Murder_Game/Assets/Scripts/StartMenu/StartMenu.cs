13deed88ff3f27b0d8e1ed992b174c2d97bc7834
DeadWOrld23 <dead.world233@gmail.com>
2025-05-25 01:09:08 +1000

    Files with MainMenu to StartMenu
    Changed file names to replace anything that contain MainMenu in it to StartMenu to keep it more consistent with naming of things since I changed MainMenu to StartMenu to help differentiate from MainScene.

--- Murder_Game/Assets/Scripts/StartMenu/StartMenu.cs
+++ Murder_Game/Assets/Scripts/StartMenu/StartMenu.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
